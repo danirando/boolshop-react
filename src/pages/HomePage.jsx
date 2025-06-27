06e22d753156dfe64ed135f9b0a57817b1a66f49
danirando <danielerando@tiscali.it>
2025-06-27 15:33:21 +0200

    fix hero-content

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,11 @@ export default function HomePage() {
   return (
     <div>
       <div className="hero-space">
-        <h1 className="hero-text">
-          Nuovi outfit, stessa vibe. Scopri la tua prossima ossessione.
-        </h1>
+        <div className="hero-content">
+          <h1 className="hero-text">
+            Nuovi outfit, stessa vibe. Scopri la tua prossima ossessione.
+          </h1>
+        </div>
       </div>
     </div>
   );
