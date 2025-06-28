cc9c959c34621d0ef02681dad64139c7e6593906
danirando <danielerando@tiscali.it>
2025-06-28 11:36:14 +0200

    add components header and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <Navbar expand="lg" className="navbar d-flex align-items-center">
+      <Container fluid>
+        <Navbar.Brand href="/">
+          <img src="../../logo.png" className="img-fluid logo" alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Link to="#" className="nav-link">
+              <i className="bi bi-cart" style={{ fontSize: "1.5rem" }}></i>
+            </Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control type="search" className="me-2" aria-label="Search" />
+            <Button className="search-button">Cerca</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,48 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function DefaultLayout() {
   return (
     <>
       <header>
-        <Navbar expand="lg" className="navbar d-flex align-items-center">
-          <Container fluid>
-            <Navbar.Brand href="/">
-              <img src="../../logo.png" className="img-fluid logo" alt="" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-              >
-                <Link to="#" className="nav-link">
-                  <i className="bi bi-cart" style={{ fontSize: "1.5rem" }}></i>
-                </Link>
-              </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button className="search-button">Cerca</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <Header />
       </header>
       <main>
         <Outlet />
       </main>
-      <footer></footer>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
