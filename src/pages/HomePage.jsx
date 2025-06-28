1bc9e4882620ec37453e523f58dc9e8b723ec3e1
danirando <danielerando@tiscali.it>
2025-06-28 14:09:18 +0200

    add cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,6 @@
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+
 export default function HomePage() {
   return (
     <div>
@@ -6,6 +9,129 @@ export default function HomePage() {
           <h1 className="hero-text">
             Nuovi outfit, stessa vibe. Scopri la tua prossima ossessione.
           </h1>
+        </div>
+      </div>
+      <div className="container sections">
+        <div className="d-flex flex-column gap-3 sections-container">
+          <section>
+            <h2>Più venduti</h2>
+            <CardGroup>
+              <Card>
+                <Card.Img
+                  className="card-img-fixed"
+                  variant="top"
+                  src="/img/a-line-skirt-black.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    <div>Prezzo:</div>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <button className="btn add-button">
+                    Aggiungi al carrello
+                  </button>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img
+                  className="card-img-fixed"
+                  variant="top"
+                  src="/img/a-line-skirt-black.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    <div>Prezzo:</div>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <button className="btn add-button">
+                    Aggiungi al carrello
+                  </button>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img
+                  className="card-img-fixed"
+                  variant="top"
+                  src="/img/a-line-skirt-black.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    <div>Prezzo:</div>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <button className="btn add-button">
+                    Aggiungi al carrello
+                  </button>
+                </Card.Footer>
+              </Card>
+            </CardGroup>
+          </section>
+          <div className="separetor"></div>
+          <section>
+            <h2>In promo</h2>
+            <CardGroup>
+              <Card>
+                <Card.Img
+                  className="card-img-fixed"
+                  variant="top"
+                  src="/img/a-line-skirt-black.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    <div>Prezzo:</div>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <button className="btn add-button">
+                    Aggiungi al carrello
+                  </button>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img
+                  className="card-img-fixed"
+                  variant="top"
+                  src="/img/a-line-skirt-black.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    <div>Prezzo:</div>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <button className="btn add-button">
+                    Aggiungi al carrello
+                  </button>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img
+                  className="card-img-fixed"
+                  variant="top"
+                  src="/img/a-line-skirt-black.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    <div>Prezzo:</div>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <button className="btn add-button">
+                    Aggiungi al carrello
+                  </button>
+                </Card.Footer>
+              </Card>
+            </CardGroup>
+          </section>
         </div>
       </div>
     </div>
