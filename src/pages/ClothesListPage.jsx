37a18f783b09f395782c376b738a9c468e01b587
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-07-02 10:10:05 +0200

    add list page

--- src/pages/ClothesListPage.jsx
+++ src/pages/ClothesListPage.jsx
@@ -1,3 +1,50 @@
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function ClothesListPage() {
-  return <h1>list</h1>;
+  const [clothes, setClothes] = useState([]);
+  const clothesUrl = import.meta.env.VITE_BOOKS_API_URL + "/clothes";
+
+  const fetchClothes = () => {
+    axios.get(clothesUrl).then((res) => {
+      console.log(res.data);
+      setClothes(res.data);
+    });
+  };
+
+  useEffect(fetchClothes, []);
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center ">Abbigliamento</h1>
+        <div className="row gy-4 ">
+          {clothes.map((cloth) => {
+            return (
+              <>
+                <div key={cloth.id} className="col-sm-12 col-md-6 col-lg-4">
+                  <div className="card h-100  ">
+                    <img
+                      src={cloth.img}
+                      alt={cloth.name}
+                      className="card-img-top h-100 "
+                    />
+
+                    <div className="card-body py-2">
+                      <h6>{cloth.name}</h6>
+                      <p>{cloth.price}€</p>
+                      <NavLink to="/clothes/:id" className="nav-item">
+                        Vedi dettagli
+                      </NavLink>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
