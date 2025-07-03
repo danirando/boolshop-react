47ca2ff9c2c648ab729ee48023c6272a744ae4da
danirando <danielerando@tiscali.it>
2025-07-03 11:33:31 +0200

    remove hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,33 +8,33 @@ export default function Header() {
     <Navbar expand="md" className="navbar d-flex align-items-center">
       <Container fluid>
         <Navbar.Brand href="/">
-          <img src="../../logo.png" className="img-fluid logo" alt="Logo" />
+          <img
+            src="../../logo.png"
+            className="img-fluid logo logo-img"
+            alt="Logo"
+          />
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: "100px" }}
+          navbarScroll>
+          <Link to="#" className="nav-link">
+            <i className="bi bi-cart" style={{ fontSize: "1.5rem" }}></i>
+          </Link>
+        </Nav>
 
-        <Navbar.Collapse style={{ height: "90px" }} id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll>
-            <Link to="#" className="nav-link">
-              <i className="bi bi-cart" style={{ fontSize: "1.5rem" }}></i>
-            </Link>
-          </Nav>
-
-          <form className="d-flex">
-            <input
-              type="search"
-              className="form-control me-2 search-input"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button type="submit" className="btn search-button">
-              Search
-            </button>
-          </form>
-        </Navbar.Collapse>
+        <form className="d-flex">
+          <input
+            type="search"
+            className="form-control me-2 search-input"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button type="submit" className="btn search-button">
+            Search
+          </button>
+        </form>
       </Container>
     </Navbar>
   );
