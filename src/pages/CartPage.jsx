a6072596cef6600fd956fec5f9a62aa26be6b472
lucioacri <lucioacri@gmail.com>
2025-07-03 12:08:58 +0200

    add context in branch

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,3 +1,7 @@
+import { useContext } from "react";
+import { useCart } from "../contexts/CartContext";
+
 export default function CartPage() {
+  const cart = useCart();
   return <h1>cart</h1>;
 }
