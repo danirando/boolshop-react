b2a97f081c832fe55ecc7039ff5924a06a557f25
danirando <danielerando@tiscali.it>
2025-07-03 14:04:46 +0200

    add badge cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import { useCart } from "../contexts/CartContext";
 
 export default function Header() {
+  const { cart } = useCart(); // prendi il carrello dal context
+  const cartCount = cart.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <Navbar expand="md" className="navbar d-flex align-items-center">
       <Container fluid>
@@ -18,10 +21,16 @@ export default function Header() {
         <Nav
           className="me-auto my-2 my-lg-0"
           style={{ maxHeight: "100px" }}
-          navbarScroll
-        >
-          <Link to="/cart" className="nav-link">
+          navbarScroll>
+          <Link to="/cart" className="nav-link position-relative">
             <i className="bi bi-cart" style={{ fontSize: "1.5rem" }}></i>
+            {cartCount > 0 && (
+              <span
+                className="start-100 translate-middle badge rounded-pill bg-danger"
+                style={{ fontSize: "0.75rem" }}>
+                {cartCount}
+              </span>
+            )}
           </Link>
         </Nav>
 
