e307e9d2b415a8451ab96a0d35e0fd9a03b09030
lucioacri <lucioacri@gmail.com>
2025-07-03 12:31:00 +0200

    add cart details

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -20,8 +20,12 @@ export function CartProvider({ children }) {
     setCart((prev) => [...prev, item]);
   };
 
+  const removeFromCart = (id) => {
+    setCart((prev) => prev.filter((item) => item.id !== id));
+  };
+
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
       {children}
     </CartContext.Provider>
   );
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,44 @@
-import { useContext } from "react";
 import { useCart } from "../contexts/CartContext";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
 export default function CartPage() {
-  const cart = useCart();
-  return <h1>cart</h1>;
+  const { cart, removeFromCart } = useCart();
+  return (
+    <div className="container mt-5">
+      <div className="my-5">
+        <h1>My cart</h1>
+      </div>
+
+      {cart.length === 0 ? (
+        <p>Cart is empty</p>
+      ) : (
+        <CardGroup className="gap-3 d-flex flex-wrap">
+          {cart.map((item) => (
+            <Card key={item.id} className="card-clothes">
+              <Card.Img
+                variant="top"
+                src={item.img}
+                className="card-img-fixed"
+              />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Text>
+                  Price: <span className="price">{item.price} €</span>
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => removeFromCart(item.id)}
+                >
+                  Remove
+                </button>
+              </Card.Footer>
+            </Card>
+          ))}
+        </CardGroup>
+      )}
+    </div>
+  );
 }
