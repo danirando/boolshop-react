164a5efa9c07b783c4b424a95a75fab6f2ea31ac
danirando <danielerando@tiscali.it>
2025-07-07 16:22:32 +0200

    no-fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,46 +35,47 @@ export default function Header() {
     <>
       <Navbar expand="md" className="navbar d-flex align-items-center">
         <Container className="d-flex header-container">
-          <Navbar.Brand href="/">
-            <img
-              src="../../logo.png"
-              className="img-fluid logo logo-img"
-              alt="Logo"
-            />
-          </Navbar.Brand>
-          <Link to="/clothes" className="nav-link clothes">
-            Clothes
-          </Link>
-
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll>
-            <Link to="/cart" className="nav-link position-relative">
-              <i className="bi bi-cart" style={{ fontSize: "1.5rem" }}></i>
-              {cartCount > 0 && (
-                <span
-                  className="start-100 translate-middle badge rounded-pill bg-danger"
-                  style={{ fontSize: "0.75rem" }}>
-                  {cartCount}
-                </span>
-              )}
+          <div className="header-row first">
+            <Navbar.Brand href="/">
+              <img
+                src="../../logo.png"
+                className="img-fluid logo logo-img"
+                alt="Logo"
+              />
+            </Navbar.Brand>
+            <Link to="/clothes" className="nav-link clothes">
+              Clothes
             </Link>
-          </Nav>
+          </div>
 
-          <form className="d-flex" onSubmit={handleSubmit}>
-            <input
-              type="search"
-              className="form-control me-2 search-input"
-              placeholder="Search"
-              aria-label="Search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button type="submit" className="btn search-button">
-              Search
-            </button>
-          </form>
+          <div className="header-row second">
+            <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+              <Link to="/cart" className="nav-link position-relative">
+                <i className="bi bi-cart" style={{ fontSize: "1.5rem" }}></i>
+                {cartCount > 0 && (
+                  <span
+                    className="start-100 translate-middle badge rounded-pill bg-danger"
+                    style={{ fontSize: "0.75rem" }}>
+                    {cartCount}
+                  </span>
+                )}
+              </Link>
+            </Nav>
+
+            <form className="d-flex" onSubmit={handleSubmit}>
+              <input
+                type="search"
+                className="form-control me-2 search-input"
+                placeholder="Search"
+                aria-label="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button type="submit" className="btn search-button">
+                Search
+              </button>
+            </form>
+          </div>
         </Container>
       </Navbar>
       <div
