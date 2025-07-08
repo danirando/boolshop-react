7a336e54da26101a1fca2cdfb10c2466a9559e1e
danirando <danielerando@tiscali.it>
2025-07-08 13:54:21 +0200

    fix 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,7 +2,7 @@ export default function NotFoundPage() {
   return (
     <>
       <div className="container my-3">
-        <h1>Errore 404. Pagina non trovata</h1>
+        <h1>Error 404. Page not found</h1>
       </div>
     </>
   );
